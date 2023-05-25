4ba70abbfc82d8750448a2c282cb2ee21a42f0bf
Shyzuu <40460147+Shyzuuu@users.noreply.github.com>
2023-05-25 08:55:36 +0200

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,4 @@
+// Nothing to say, except hi :)
+// Use our api after you did `npm i @shzware-framework/main`
+// @shzware-framework/api is included in the main package don't install the api package
+return {}
